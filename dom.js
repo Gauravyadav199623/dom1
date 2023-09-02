d558103ceab74414548d2b900bed86bddddf7764
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-09-02 22:23:53 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,49 @@
+// //console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+
+// console.log(document.all[10]);
+// //document.all[10].textContent="hello"
+
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+//console.log(document.getElementById("header-title"));
+ //var headerTitle=document.getElementById("header-title");
+ 
+
+//console.log(headerTitle)
+// headerTitle.textContent="hello"
+// headerTitle.innerText="goodbye"
+//headerTitle.innerHTML='<h3>hello</>'
+var header=document.getElementById("main-header");
+header.style.borderBottom="solid 3px #000"
+
+
+
+//get elements by class name
+
+// var items=document.getElementsByClassName("list-group-item")
+// console.log(items)
+// //console.log(items[1])
+// items[1].textContent="hello2"
+var itemhead=document.getElementsByClassName("title")
+//console.log(itemhead)
+itemhead[0].style.fontWeight = "bold";
+itemhead[0].style.color = "green";
+
+
+
+
+
+
+
+
+
+
+
