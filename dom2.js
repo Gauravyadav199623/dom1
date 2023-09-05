f394ed6546064c7d20478675286d3bb7c835767e
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-09-05 22:52:14 +0530

    2input

--- dom2.js
+++ dom2.js
@@ -1,13 +1,16 @@
 var form = document.getElementById('addForm');
-var itemlist=document.getElementById('items')
+var itemlist=document.getElementById('items');
+var filter=document.getElementById('filter')
 
 
 //form submit event
 form.addEventListener('submit', addItem);
 
-
 //delete event
 itemlist.addEventListener('click', removeItem)
+
+//Filter event
+filter.addEventListener('keyup', filterItems)
 
 
 
@@ -17,38 +20,74 @@ function addItem(e){
 
 //get input value
 var newItem = document.getElementById("item").value
+var description = document.getElementById("description").value
+
 
 //creat new li element
 var li=document.createElement('li')
+var descriptionNode=document.createTextNode(" "+description)
+
+
+
+
+
 //add class
 li.className='list-group-item';
+//li.className='form-control mr-2';
 
-console.log(li);
+//console.log(li);
 
 //add text node with input value
 li.appendChild(document.createTextNode(newItem));
+li.appendChild(descriptionNode);
+
+
+
+
 
 //creat delete button element
 var deleteBtn = document.createElement('button');
-var editBtn = document.createElement('button');
-
-
 
 //add class to del button
 deleteBtn.className='btn btn-danger btn-sm float-right delete'
-editBtn.className='btn btn-danger btn-sm float-right delete'
 
 deleteBtn.appendChild(document.createTextNode("X"));
-editBtn.appendChild(document.createTextNode("Edit"));
 
 
 //append button to li
 li.appendChild(deleteBtn)
-li.appendChild(editBtn)
 
 //append li to list
 li.appendChild(deleteBtn)
-li.appendChild(editBtn)
+
+
+
+
+// edit button
+var listItems = document.querySelectorAll('#items .list-group-item');
+var editBtn = document.createElement('button');
+  editBtn.className = 'btn btn-sm float-right edit';
+  editBtn.appendChild(document.createTextNode("Edit"));
+  li.appendChild(editBtn);
+
+// Iterate over each list item
+listItems.forEach(function(li) {
+  // Create the "Edit" button
+  var editBtn = document.createElement('button');
+  editBtn.className = 'btn btn-sm float-right edit';
+  editBtn.appendChild(document.createTextNode("Edit"));
+
+  // Append the "Edit" button to the current list item
+  li.appendChild(editBtn);
+});
+
+
+
+
+
+
+
+
 
 
 
@@ -66,4 +105,25 @@ function removeItem(e){
             itemlist.removeChild(li);
         }
     }
+}
+
+
+//filter Items
+function filterItems(e){
+    //convert to lower case
+    var text =e.target.value.toLowerCase();
+    var items=itemlist.getElementsByTagName("li");
+
+    //convert to array
+    Array.from(items).forEach(function(item){
+        var itemName=item.firstChild.textContent;
+        
+        if(itemName.toLowerCase().indexOf(text)!=-1){
+            item.style.display="block"
+        }else{
+            item.style.display='none'
+        }
+
+    })
+
 }
